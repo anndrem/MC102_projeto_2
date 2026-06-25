1d7ae8ddef8757ad7b48e1fb13ef41b073fac1a0
andre <dealmeida.andre08@gmail.com>
2026-06-25 00:04:01 -0300

    feat: ordenando as cartas ao inicio da rodada

--- truco-main/student_players.py
+++ truco-main/student_players.py
@@ -11,83 +11,59 @@ RESPOSTA = {'correr': 0, 'aceitar': 1, 'aumentar': 2}
 ORDER_CARDS = ['4' ,'5' ,'6' ,'7' ,'Q' ,'J' ,'K' ,'A' ,'2' ,'3']
 ORDER_NIPES = ['Diamonds', 'Spades', 'Hearts', 'Clubs']
 
-def sortCards(cards):
-    c1, n1 = cards[0][0], cards[0][1]
-    c2, n2 = cards[1][0], cards[1][1]
-    c3, n1 = cards[2][0], cards[2][1]
-
-    return c1, n1
-
-
 class SmartPlayer(Player):
+    
     def __init__(self, ra, name):
         super().__init__(ra, name) 
-
         self._respond = RESPOSTA['aceitar']
 
-    def theresManilha(self, cards, top_card):
+    def _theresManilha(self,top_card):
         manilhas = {}   
-        
-        idx_c1, idx_n1 = ORDER_CARDS.index(cards[0][0]), ORDER_NIPES.index(cards[0][1])
-        idx_c2, idx_n2 = ORDER_CARDS.index(cards[1][0]), ORDER_NIPES.index(cards[1][1])
-        idx_c3, idx_n3 = ORDER_CARDS.index(cards[2][0]), ORDER_NIPES.index(cards[2][1])
+        idx = -1
 
-        for key, value in {idx_c1: idx_n1, idx_c2: idx_n2, idx_c3: idx_n3}.items():
+        idx_c1, idx_n1 = ORDER_CARDS.index(self.cards[0][0]), ORDER_NIPES.index(self.cards[0][1])
+        idx_c2, idx_n2 = ORDER_CARDS.index(self.cards[1][0]), ORDER_NIPES.index(self.cards[1][1])
+        idx_c3, idx_n3 = ORDER_CARDS.index(self.cards[2][0]), ORDER_NIPES.index(self.cards[2][1])
+
+        for key, value in [[idx_c1, idx_n1], [idx_c2, idx_n2], [idx_c3, idx_n3]]:
             if key == ORDER_CARDS.index(top_card[0]) + 1:
+                idx = self.cards.index((ORDER_CARDS[key], ORDER_NIPES[value]))
                 manilhas[ORDER_CARDS[key]] = ORDER_NIPES[value]
 
-        return True if len(manilhas) else False
-    def sortCards(self, cards, top_card = None):
-        if len(cards) < 3:
+
+        return (True, idx) if idx != -1 else (False, -1)
+    
+    def _sortCards(self):
+        if len(self.cards) < 3:
             return None
         
-        c1, n1 = cards[0][0], cards[0][1]
-        c2, n2 = cards[1][0], cards[1][1]
-        c3, n3 = cards[2][0], cards[2][1]
+        idx_c1, idx_n1 = ORDER_CARDS.index(self.cards[0][0]), ORDER_NIPES.index(self.cards[0][1])
+        idx_c2, idx_n2 = ORDER_CARDS.index(self.cards[1][0]), ORDER_NIPES.index(self.cards[1][1])
+        idx_c3, idx_n3 = ORDER_CARDS.index(self.cards[2][0]), ORDER_NIPES.index(self.cards[2][1])
+
+        verification_idx = [[idx_c1, idx_n1], [idx_c2, idx_n2], [idx_c3, idx_n3]]
+        verification_idx.sort(key= lambda x: x[0], reverse=True)
         
-        player_cards = {c1: n1, c2: n2, c3: n3}
-
-        idx_c1 = ORDER_CARDS.index(cards[0][0])
-        idx_c2 = ORDER_CARDS.index(cards[1][0])
-        idx_c3 = ORDER_CARDS.index(cards[2][0])
-
-        verification_idx = [idx_c1, idx_c2, idx_c3]
-        
-        idx_best_card = max(verification_idx)
-        verification_idx.remove(idx_best_card)
-
-        idx_worst_card = min(verification_idx)
-        verification_idx.remove(idx_worst_card)
-        
-        best_card = ORDER_CARDS[idx_best_card]
-        mid_card = ORDER_CARDS[verification_idx[0]]
-        worst_card = ORDER_CARDS[idx_worst_card]
-        
-        # basic_sort = [best_card, mid_card, worst_card]
-        # changes_cards = basic_sort.copy()
-
-        # if len(manilhas) >= 0:
-        #     for i in range(len(manilhas)):
-        #         if basic_sort[i] != manilhas[i]:
-        #             changes_cards[changes_cards.index(manilhas[i])] = basic_sort[i]
-        #             basic_sort[i] = manilhas[i]
-
-        # print(f'basic = {basic_sort}')
-        # print(f'change = {changes_cards}')
-        print(f'manilhas em mao: {manilhas}')
-        sorted_cards = [(best_card, player_cards[best_card]), (mid_card, player_cards[mid_card]), (worst_card, player_cards[worst_card])]
+        sorted_cards = [(ORDER_CARDS[verification_idx[0][0]], ORDER_NIPES[verification_idx[0][1]]), (ORDER_CARDS[verification_idx[1][0]], ORDER_NIPES[verification_idx[1][1]]), (ORDER_CARDS[verification_idx[2][0]], ORDER_NIPES[verification_idx[2][1]])]
         return sorted_cards
 
     '''O JOGO ESTA DEFINIDO AQUI'''
     def play(self, top_card, play_hist, score_hist):
-
+        
+        manilha = (False, -1)
         if len(self.cards) == 3:
-            print(f'vira: {top_card}')
-            print(f'old cards: {self.cards}')
-            sorted_cards = self.sortCards(self.cards, top_card)
+            # print(f'CARTAS NAO ORDENADAS:\n--{self.name}: {self.cards}')
+            manilha = self._theresManilha(top_card)
+            self.cards = self._sortCards()
+            # print(f'CARTAS ORDENADAS:\n--{self.name}: {self.cards}')
 
+
+        if manilha[0]:
+            idx_manilha = manilha[1]
+            print(f'JOGANDO MANILHA:\n--vira{top_card}: {self.cards[idx_manilha]}')
+            return DECISAO['normal'], self.cards[idx_manilha]
         if self._cards:
-            return DECISAO['normal'], self._cards[0]
+            return DECISAO['normal'], self.cards[0]
             
         return 1, None
 
